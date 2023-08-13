ffdf43284e0b6ea591950b0535b354c529c4eb39
Harsh-Hotwani <122560845+Harsh-Hotwani@users.noreply.github.com>
2023-08-13 12:09:45 +0530

    1st commit

---
+++ app/src/main/java/com/example/lottie_animation/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.lottie_animation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
